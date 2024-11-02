92f97294408298dc2ead5c585c6f573e6ab28bd4
Fizaa2007 <fizaamuhammadfareed@gmail.com>
2024-11-02 06:44:55 -0700

    Add files via upload

---
+++ First.py
@@ -0,0 +1,67 @@
+#MID PROJECT
+#GROUP MEMBERS:
+#FIZA
+#SUMBUL
+#SAMIYA
+#PROJECT NAME:create quize app with multiple choice question
+
+#multiple choice quize
+ques1="MS word is software?"
+A="application"
+B="complier"
+C="system"
+print("answer1: application")
+
+ques2="whichis not in MS word?"
+A="italic"
+B="magic tool"
+C="font"
+print("answer2:magic tool")
+
+ques3="which is not an edition of MS word?"
+A="MS word 2003"
+B="MS word 2007"
+C="MS word 1020"
+print("answer3: MS word1020")
+
+ques4="formating is performed on?"
+A="text"
+B="table"
+c="both(A) and (B)"
+print("answer:4 both(A) and (B)" )
+
+ques5="which type of programming does python support?"
+A="object-oriented programming"
+B="structured programming"
+C="all of the mentioned"
+print("answer5:all of the mentioned")
+
+ques6="is python case sensitive when dealing with indentifiers?"
+A="no"
+B="yes"
+C="none of the mentioned"
+print("answer6: yes")
+
+ques7="which of the followingis the correct extension of the python file?"
+A="python"
+B="pi"
+C="py"
+print("answer7: py")
+
+ques8="which keyword is used for functioning in python language?"
+A="fuction"
+B="def"
+C="fun"
+print("anwer8: def")
+
+ques9="which one of the following is not a keyword in python language? "
+A="assert"
+B="pass"
+C="eval"
+print("answer9: eval")
+
+ques10="which module inthe python standard library parses option received from the command line?"
+A="getarg"
+B="getopt"
+C="main"
+print("anwer10: getopt")
